0a98ea3b073adffb8f87198148219ef77ba54425
Administrator <administrator@local>
2026-09-11 17:29:16 +0700

    sync: update from trahsukata

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/trahsukata/',
+  base: '/silsilah-demo/',
   plugins: [react(), tailwindcss()],
 });
